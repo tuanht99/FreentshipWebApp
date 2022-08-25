ebaded3096e6f09becaffccc3ac2fc255655f3fb
thanhlk0803 <20211TT1281@mail.tdc.edu.vn>
2022-08-25 19:45:48 +0700

    sủa tên file

--- Freentship/App.js
+++ Freentship/App.js
@@ -1,7 +1,7 @@
 import {  View } from 'react-native'
 
 import React from 'react'
-import Notify from './view/notify'
+import Notify from './views/NotifyView'
 // import InforUserSettings from './view/InforSetting'
 // import RootNavigator from './navigators/RootNavigator';
 export default function App() {
