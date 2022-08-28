76ef2d49c9d66bdb556b8ad661ea53b48df099cb
thanhlk0803 <20211TT1281@mail.tdc.edu.vn>
2022-08-28 16:57:20 +0700

    thay đổi theme

---
+++ Freentship/themes/InforUserSettingTheme.js
@@ -0,0 +1,100 @@
+import { StyleSheet,  flex, } from 'react-native'
+
+
+const InforSettingView = StyleSheet.create({
+    container: {
+      flex: 1,
+      display: 'flex',
+      backgroundColor: "#DCDCDC",
+      width: '100%',
+      height: '100%',
+    },
+    title: {
+      color: 'red',
+      alignContent: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    navigators: {
+      margin: 5,
+      flex: 1,
+      display: 'flex',
+      paddingTop: 10,
+      flexDirection: 'row',
+      paddingTop: 30,
+      backgroundColor: "white",
+  
+    },
+    avatar: {
+      margin: 5,
+      flex: 3,
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: "white",
+  
+    },
+    Profile: {
+      margin: 5,
+      flex: 3,
+      display: 'flex',
+  
+      backgroundColor: 'white',
+      flexDirection: 'column',
+    },
+  
+    ChangePassword: {
+      margin: 5,
+      flex: 1,
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+    },
+    navigatorsText: {
+      paddingLeft: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: 30,
+  
+    },
+    Image: {
+      paddingTop: 50,
+      width: 50,
+      height: 50,
+    },
+    avatarchange: {
+      height: 80, width: 80,
+      resizeMode: "contain",
+      borderRadius: 50,
+    },
+    profileText: {
+      fontSize: 20,
+      textAlign: 'center',
+      paddingTop: 10,
+    },
+    
+    textProfile: {
+      marginLeft: 130,
+      fontSize: 20,
+      color: 'blue',
+    },
+    touchableOpacityChangePassword: {
+      paddingLeft: 150,
+    },
+    Phone: {
+      flex: 2,
+      margin: 5,
+      backgroundColor: 'white',
+    },
+    PhoneText: {
+      fontSize: 20,
+      textAlign: 'center',
+      paddingTop: 10,
+      color: 'black',
+    },
+    textPhone: {
+      marginLeft: 110,
+      fontSize: 20,
+      color: 'blue',
+    },
+  });
+  export default InforSettingView;
--- Freentship/views/InforSettingView.js
+++ Freentship/views/InforSettingView.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity, Button, flex, } from 'react-native'
+import { Text, View, Image, TouchableOpacity, } from 'react-native'
 import React from 'react'
 // list dữ liệu
 import Avatar from '../assets/gaubeo.jpg';
@@ -10,27 +10,27 @@ const PROFICLE = [{
   image: Avatar,
 
 }]
-
+import AppStyle from '../themes/InforUserSettingTheme'
 export default function InforSettingView() {
   return (
 
-    <View style={styles.container}>
+    <View style={AppStyle.container}>
       {/* navigators  */}
-      <View style={styles.navigators}>
+      <View style={AppStyle.navigators}>
         <TouchableOpacity>
-          <Image style={styles.Image} source={require('../assets/arrow-left.png')}></Image>
+          <Image style={AppStyle.Image} source={require('../assets/arrow-left.png')}></Image>
         </TouchableOpacity>
-        <Text style={styles.navigatorsText}>
+        <Text style={AppStyle.navigatorsText}>
           Quản Lí Hồ Sơ
         </Text>
       </View>
       {/* avatars */}
-      <View style={styles.avatar}>
+      <View style={AppStyle.avatar}>
         <TouchableOpacity>
           {PROFICLE.map((user) => (
             <View style={""}>
               <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-                <Image style={styles.avatarchange} source={user.image} />
+                <Image style={AppStyle.avatarchange} source={user.image} />
               </View>
 
               <View style={{ flexDirection: 'column', flex: 4 }}>
@@ -53,29 +53,29 @@ export default function InforSettingView() {
         </TouchableOpacity>
       </View>
       {/* Profile */}
-      <View style={styles.Profile}>
+      <View style={AppStyle.Profile}>
         {/* nút thay đổi */}
         <View style={{ flexDirection: 'row', }} >
 
-          <Text style={styles.profileText}>
+          <Text style={AppStyle.profileText}>
             Thông tin cá nhân
           </Text>
-          <TouchableOpacity style={styles.touchableOpacityProfile}>
-            <Text style={styles.textProfile}>thay đổi</Text>
+          <TouchableOpacity style={AppStyle.touchableOpacityProfile}>
+            <Text style={AppStyle.textProfile}>thay đổi</Text>
           </TouchableOpacity>
         </View>
         <View >
           <View style={{ flexDirection: 'row', }} >
-            <Image style={styles.Image} source={require('../assets/usericon.jpg')}></Image>
-            <Text style={styles.profileText} >thanh trần nhật</Text>
+            <Image style={AppStyle.Image} source={require('../assets/usericon.jpg')}></Image>
+            <Text style={AppStyle.profileText} >thanh trần nhật</Text>
           </View>
           <View style={{ flexDirection: 'row', }} >
-            <Image style={styles.Image} source={require('../assets/mailicon.png')}></Image>
-            <Text style={styles.profileText}>thanh126126@gmail.com</Text>
+            <Image style={AppStyle.Image} source={require('../assets/mailicon.png')}></Image>
+            <Text style={AppStyle.profileText}>thanh126126@gmail.com</Text>
           </View>
           <View style={{ flexDirection: 'row', }} >
-            <Image style={styles.Image} source={require('../assets/calendaricon.jpg')}></Image>
-            <Text style={styles.profileText}>08/03/2002</Text>
+            <Image style={AppStyle.Image} source={require('../assets/calendaricon.jpg')}></Image>
+            <Text style={AppStyle.profileText}>08/03/2002</Text>
           </View>
 
 
@@ -86,33 +86,33 @@ export default function InforSettingView() {
 
       </View>
       {/* phone */}
-      <View style={styles.Phone}>
+      <View style={AppStyle.Phone}>
         <View style={{ flexDirection: 'row', }} >
 
-          <Text style={styles.PhoneText}>
-              Số điện thoại liên lạc
+          <Text style={AppStyle.PhoneText}>
+            Số điện thoại liên lạc
           </Text>
-          <TouchableOpacity style={styles.touchableOpacityProfile}>
-            <Text style={styles.textPhone}>thay đổi</Text>
+          <TouchableOpacity style={AppStyle.touchableOpacityProfile}>
+            <Text style={AppStyle.textPhone}>thay đổi</Text>
           </TouchableOpacity>
         </View>
         <View>
-        <View style={{ flexDirection: 'row',paddingTop:20 }} >
-            <Image style={styles.Image} source={require('../assets/telephone.png')}></Image>
-            <Text style={styles.profileText} >0123456789</Text>
+          <View style={{ flexDirection: 'row', paddingTop: 20 }} >
+            <Image style={AppStyle.Image} source={require('../assets/telephone.png')}></Image>
+            <Text style={AppStyle.profileText} >0123456789</Text>
           </View>
         </View>
       </View>
       {/* change password */}
-      <View style={styles.ChangePassword}>
+      <View style={AppStyle.ChangePassword}>
         <View style={{ flexDirection: 'row', }} >
-          <Image style={styles.Image} source={require('../assets/padlock.png')}></Image>
+          <Image style={AppStyle.Image} source={require('../assets/padlock.png')}></Image>
 
-          <Text style={styles.profileText}>
+          <Text style={AppStyle.profileText}>
             đổi mật khẩu
           </Text>
-          <TouchableOpacity style={styles.touchableOpacityChangePassword}>
-            <Image style={styles.Image} source={require('../assets/right-arrow.png')}></Image>
+          <TouchableOpacity style={AppStyle.touchableOpacityChangePassword}>
+            <Image style={AppStyle.Image} source={require('../assets/right-arrow.png')}></Image>
           </TouchableOpacity>
         </View>
       </View>
@@ -120,99 +120,4 @@ export default function InforSettingView() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    backgroundColor: "#DCDCDC",
-    width: '100%',
-    height: '100%',
-  },
-  title: {
-    color: 'red',
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  navigators: {
-    margin: 5,
-    flex: 1,
-    display: 'flex',
-    paddingTop: 10,
-    flexDirection: 'row',
-    paddingTop: 30,
-    backgroundColor: "white",
 
-  },
-  avatar: {
-    margin: 5,
-    flex: 3,
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: "white",
-
-  },
-  Profile: {
-    margin: 5,
-    flex: 3,
-    display: 'flex',
-
-    backgroundColor: 'white',
-    flexDirection: 'column',
-  },
-
-  ChangePassword: {
-    margin: 5,
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  navigatorsText: {
-    paddingLeft: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 30,
-
-  },
-  Image: {
-    paddingTop: 50,
-    width: 50,
-    height: 50,
-  },
-  avatarchange: {
-    height: 80, width: 80,
-    resizeMode: "contain",
-    borderRadius: 50,
-  },
-  profileText: {
-    fontSize: 20,
-    textAlign: 'center',
-    paddingTop: 10,
-  },
-  
-  textProfile: {
-    marginLeft: 130,
-    fontSize: 20,
-    color: 'blue',
-  },
-  touchableOpacityChangePassword: {
-    paddingLeft: 150,
-  },
-  Phone: {
-    flex: 2,
-    margin: 5,
-    backgroundColor: 'white',
-  },
-  PhoneText: {
-    fontSize: 20,
-    textAlign: 'center',
-    paddingTop: 10,
-    color: 'black',
-  },
-  textPhone: {
-    marginLeft: 110,
-    fontSize: 20,
-    color: 'blue',
-  },
-});
